d7c4bb339dde46f0f5cf8c254f71efe4c4a16a77
= <slotuser@gmail.com>
2020-02-28 14:32:27 +0100

    Player: del collider + refacto.

--- src/prefabs/player.ts
+++ src/prefabs/player.ts
@@ -1,67 +1,51 @@
-/**
-* Player (character) class (constructor)
-* Animated : yes. Sprite: yes. Physics: yes.
-* Number of instances expected in final game: 2.
-**/
-
 export class Player extends Phaser.Physics.Arcade.Sprite {
 
-	private jumpKey:Phaser.Input.Keyboard.Key;
-	private rightKey:Phaser.Input.Keyboard.Key;
-	private leftKey:Phaser.Input.Keyboard.Key;
+	private jumpKey: Phaser.Input.Keyboard.Key;
+	private rightKey: Phaser.Input.Keyboard.Key;
+	private leftKey: Phaser.Input.Keyboard.Key;
+	private isDead: boolean;
+	private life: number;
+	private vx: number;
+	private gravityY: number;
+	private jumpVelocity: number;
+	private bounce: number;
+	private jumpSound: Phaser.Sound.BaseSound;
 
-	private isDead:boolean;
-
-	private vx:number;
-	private gravityY:number;
-	private jumpVelocity:number;
-	private bounce:number;
-
-	private jumpSound:Phaser.Sound.BaseSound;
-
-	public getDead():boolean {
+	public getDead(): boolean {
 		return this.isDead;
 	}
 
-	public setDead(pDead:boolean):void {
+	public setDead(pDead: boolean): void {
 		this.isDead = pDead;
 	}
 
+
 	constructor(params) {
+
 		super(params.scene, params.x, params.y, params.key, params.frame);
 
 		this.isDead = false;
+		this.life = 100;
 		this.gravityY = 50;
 		this.jumpVelocity = -600;
 		this.vx = 150;
 		this.bounce = 0.5;
+		this.jumpSound = this.scene.sound.add('jumpSound');
 
-		this.jumpSound = params.scene.sound.add('jumpSound');
-
-		// Sets 'this.body' (player body) to not null
+		// Sets 'this.body' (player body --> used in this.update) to not null
 		this.scene.physics.world.enable(this);
 
 		this.setGravityY(this.gravityY);
 		this.setBounce(this.bounce);
 		this.setCollideWorldBounds(true);
-		/* map.tiles is the public staticGroup declared in the GameMap class 
-		from where each tile texture is drawn (with create() : see map.ts) so 
-		that's the group which is colliding with the player */
-		params.scene.physics.add.collider(params.scene.map.tiles, this);
 
-		this.jumpKey = this.scene.input.keyboard.addKey(
-			params.controlKeys['jump']
-		);
-		this.rightKey = this.scene.input.keyboard.addKey(
-			params.controlKeys['right']
-		);
-		this.leftKey = this.scene.input.keyboard.addKey(
-			params.controlKeys['left']
-		);
+		this.jumpKey = this.scene.input.keyboard.addKey(params.controlKeys['jump']);
+		this.rightKey = this.scene.input.keyboard.addKey(params.controlKeys['right']);
+		this.leftKey = this.scene.input.keyboard.addKey(params.controlKeys['left']);
 
-		params.scene.anims.create({
+		this.scene.anims.create({
 			key: 'left',
-			frames: params.scene.anims.generateFrameNumbers('player', {
+			frames: this.scene.anims.generateFrameNumbers('player', {
 				start: 0,
 				end: 3
 			}),
@@ -69,7 +53,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 			repeat: -1
 		});
 
-		params.scene.anims.create({
+		this.scene.anims.create({
 			key: 'turn',
 			frames: [{
 				key: 'player',
@@ -78,9 +62,9 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 			frameRate: 20
 		});
 
-		params.scene.anims.create({
+		this.scene.anims.create({
 			key: 'right',
-			frames: params.scene.anims.generateFrameNumbers('player', {
+			frames: this.scene.anims.generateFrameNumbers('player', {
 				start: 5,
 				end: 8
 			}),
@@ -92,7 +76,8 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 		this.scene.add.existing(this);
 	}
 
-	update():void {
+	update(): void {
+
 		if (this.leftKey.isDown) {
 			this.setVelocityX(-this.vx);
 			this.anims.play('left', true);
@@ -110,5 +95,6 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 			this.setVelocityY(this.jumpVelocity);
 			this.jumpSound.play();
 		}
+		
 	}
 }
