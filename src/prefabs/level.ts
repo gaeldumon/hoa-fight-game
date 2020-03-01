42db4c5560b480ae330b332d41be7f777695ec0e
= <slotuser@gmail.com>
2020-03-01 20:27:16 +0100

    Renamed gameMap/GameMap to level/Level.

--- src/prefabs/level.ts
+++ src/prefabs/level.ts
@@ -37,6 +37,7 @@ export class Level {
 		
 		this.tileWidth = 32;
 		this.tileHeight = 32;
+		
 		// Using params cause class doesn't extends to anything so we can't super() the scene and write this.scene
 		this.tiles = params.scene.physics.add.staticGroup();
 
