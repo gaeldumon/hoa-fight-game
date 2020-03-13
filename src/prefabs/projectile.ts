be68e37b55c5c6bc740b29c5e4bce9ecfcdfcb8a
= <slotuser@gmail.com>
2020-03-13 15:22:44 +0100

    Combined phasers's setVx and setVy of projectile into one setV()

--- src/prefabs/projectile.ts
+++ src/prefabs/projectile.ts
@@ -3,20 +3,16 @@ import { getGameWidth, getGameHeight } from '../helpers';
 export class Projectile extends Phaser.Physics.Arcade.Image {
 	private vx: number;
 	private vy: number;
-	private accelX: number;
 
 	private initPhysics(params): void {
 		this.vx = params.vx;
 		this.vy = params.vy;
-		this.accelX = 400;
 	}
 
 	private applyPhysics(): void {
 		// Sets projectile body to not null
 		this.scene.physics.world.enable(this);
-		this.setAccelerationX(this.accelX);
-		this.setVelocityX(this.vx);
-		this.setVelocityY(this.vy);
+		this.setVelocity(this.vx, this.vy);
 	}
 
 	constructor(params) {
