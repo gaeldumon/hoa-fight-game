b2f76a59f40b41074abdfbf9d1997315da8ed368
= <slotuser@gmail.com>
2020-03-20 20:16:57 +0100

    Level: tried a more phaser-way to display and handle tilemap (commented out).

--- src/objects/level.ts
+++ src/objects/level.ts
@@ -6,9 +6,13 @@ export class Level {
 	private rawGrid: string;
 	private tileWidth: number;
 	private tileHeight: number;
-	public tiles: Phaser.Physics.Arcade.Group;
+	public layer: Phaser.Physics.Arcade.Group;
 
 	constructor(params: { scene: any; id: string; }) {
+		/*this.map = params.scene.make.tilemap({key: "level1", tileWidth: 32, tileHeight: 32});
+		this.tileset = this.map.addTilesetImage("graveyardTilesheet");
+		this.layer = this.map.createStaticLayer(0, this.tileset, 0, 0);
+		this.layer.setCollisionBetween(0, 5);*/
 
 		this.rawGrid = loadFile('assets/levels/level' + params.id + '.txt');
 		this.grid = this.rawGrid.split('\n').map(item => item.split(''));
@@ -17,7 +21,7 @@ export class Level {
 		this.tileHeight = 32;
 		
 		// Using params cause class doesn't extends to anything so we can't super() the scene and write this.scene
-		this.tiles = params.scene.physics.add.staticGroup();
+		this.layer = params.scene.physics.add.staticGroup();
 
 		for (let line = 0; line < this.grid.length; line++) {
 
@@ -25,7 +29,7 @@ export class Level {
 
 				if (this.grid[line][col] !== '0') {
 
-					this.tiles.create(
+					this.layer.create(
 						col * this.tileWidth + (this.tileWidth / 2), 
 						line * this.tileHeight - (this.tileHeight / 2), 
 						'ground'
