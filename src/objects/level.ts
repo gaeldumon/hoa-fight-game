88957a41e00e8e226552e00854683288f328c2d3
= <slotuser@gmail.com>
2020-03-21 14:39:21 +0100

    Level: going to a more phaser-respect way of making a tilemap (use of tilemap, tileset, staticlayer etc.).

--- src/objects/level.ts
+++ src/objects/level.ts
@@ -1,42 +1,28 @@
-import { loadFile } from '../helpers';
+import { getGameWidth, getGameHeight } from '../helpers';
 
 export class Level {
 
-	private grid: string[][];
-	private rawGrid: string;
-	private tileWidth: number;
-	private tileHeight: number;
-	public layer: Phaser.Physics.Arcade.Group;
+	private background: Phaser.GameObjects.Image;
+	private map: Phaser.Tilemaps.Tilemap;
+	private tileset: Phaser.Tilemaps.Tileset;
+	public layer: Phaser.Tilemaps.StaticTilemapLayer;
 
 	constructor(params: { scene: any; id: string; }) {
-		/*this.map = params.scene.make.tilemap({key: "level1", tileWidth: 32, tileHeight: 32});
-		this.tileset = this.map.addTilesetImage("graveyardTilesheet");
+
+		this.background = params.scene.add.image(
+			getGameWidth(params.scene)/2, 
+			getGameHeight(params.scene)/2, 
+			`background${params.id}`
+		);
+
+		this.map = params.scene.make.tilemap({
+			key: `level${params.id}`,
+			tileWidth: 32, 
+			tileHeight: 32
+		});
+
+		this.tileset = this.map.addTilesetImage(`tilesheet${params.id}`);
 		this.layer = this.map.createStaticLayer(0, this.tileset, 0, 0);
-		this.layer.setCollisionBetween(0, 5);*/
-
-		this.rawGrid = loadFile('assets/levels/level' + params.id + '.txt');
-		this.grid = this.rawGrid.split('\n').map(item => item.split(''));
-		
-		this.tileWidth = 32;
-		this.tileHeight = 32;
-		
-		// Using params cause class doesn't extends to anything so we can't super() the scene and write this.scene
-		this.layer = params.scene.physics.add.staticGroup();
-
-		for (let line = 0; line < this.grid.length; line++) {
-
-			for (let col = 0; col < this.grid[line].length; col++) {
-
-				if (this.grid[line][col] !== '0') {
-
-					this.layer.create(
-						col * this.tileWidth + (this.tileWidth / 2), 
-						line * this.tileHeight - (this.tileHeight / 2), 
-						'ground'
-					);
-
-				}
-			}
-		}
+		this.layer.setCollisionBetween(0, 14);
 	}
 }
