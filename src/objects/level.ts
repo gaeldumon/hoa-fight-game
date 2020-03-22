d465d74d0e73090ca4007c620327c76b60d0a02b
= <slotuser@gmail.com>
2020-03-22 18:40:14 +0100

    Implemented Tiled Tilemap (with layers) + easy refactoring.

--- src/objects/level.ts
+++ src/objects/level.ts
@@ -5,7 +5,17 @@ export class Level {
 	private background: Phaser.GameObjects.Image;
 	private map: Phaser.Tilemaps.Tilemap;
 	private tileset: Phaser.Tilemaps.Tileset;
-	public layer: Phaser.Tilemaps.StaticTilemapLayer;
+	// mainLayer is the layer that collides with its environment: players etc.
+	private mainLayer: Phaser.Tilemaps.StaticTilemapLayer;
+	// subLayer is the "decorative" layer, cosmetic only
+	private subLayer: Phaser.Tilemaps.StaticTilemapLayer
+
+	/**
+	 * Getter for the layer that collides: collides set to true in Tiled.
+	 */
+	getMainLayer(): Phaser.Tilemaps.StaticTilemapLayer {
+		return this.mainLayer;
+	}
 
 	constructor(params: { scene: any; id: string; }) {
 
@@ -15,8 +25,27 @@ export class Level {
 			`background${params.id}`
 		);
 
-		this.map = params.scene.make.tilemap({ key: `level${params.id}` });
-		this.tileset = this.map.addTilesetImage(`tilesheet${params.id}`);
-		this.layer = this.map.createStaticLayer(0, this.tileset, 0, 0);
+		this.map = params.scene.make.tilemap({ 
+			key: `level${params.id}` 
+		});
+
+		this.tileset = this.map.addTilesetImage(
+			`tilesheet${params.id}-32x32`, 
+			`tilesheet${params.id}`
+		);
+
+		this.mainLayer = this.map.createStaticLayer(
+			'mainLayer', 
+			this.tileset, 0, 0
+		);
+
+		this.mainLayer.setCollisionByProperty({ 
+			collides: true 
+		});
+
+		this.subLayer = this.map.createStaticLayer(
+			'subLayer', 
+			this.tileset, 0, 0
+		);
 	}
 }
