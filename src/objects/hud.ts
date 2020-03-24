b8aa53b93fc22673cd1ffb7fdca822ebb3d7713a
= <slotuser@gmail.com>
2020-03-24 19:01:07 +0100

    Created HUD class.

---
+++ src/objects/hud.ts
@@ -0,0 +1,15 @@
+/**
+ * This hud class will display the player's information in-game: its health,
+ * its avatar and rank (from website personal account).
+ * Each HUD will be linked to its corresponding player (1 HUD instance per player).
+ */
+export class Hud {
+
+	private opacity: number;
+	private textSize: number;
+	private textColor: string;
+
+	constructor(params) {
+
+	}
+}
