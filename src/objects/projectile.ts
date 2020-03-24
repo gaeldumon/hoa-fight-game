69bc7b6b745530b6dfc77233f57a098b6a4baf11
= <slotuser@gmail.com>
2020-03-24 18:50:28 +0100

    Factorized the out-of-world check of projectile.

--- src/objects/projectile.ts
+++ src/objects/projectile.ts
@@ -2,31 +2,33 @@ import { getGameWidth, getGameHeight } from '../helpers';
 
 export class Projectile extends Phaser.Physics.Arcade.Image {
 	private vx: number;
-	private vy: number;
+	private direction: number;
 
-	private initPhysics(params): void {
-		this.vx = params.vx;
-		this.vy = params.vy;
-	}
-
-	private applyPhysics(): void {
-		// Sets projectile body to not null
-		this.scene.physics.world.enable(this);
-		this.setVelocity(this.vx, this.vy);
+	private isOut(): boolean {
+		if (this.x > getGameWidth(this.scene) || 
+		this.y > getGameHeight(this.scene) || 
+		this.x < 0 || this.y < 0) {
+			return true;
+		}
+		return false;
 	}
 
 	constructor(params) {
 		super(params.scene, params.x, params.y, params.textureKey);
-		this.initPhysics(params);
-		this.applyPhysics();
+
+		this.vx = 1500;
+		this.direction = params.direction;
+
+		this.scene.physics.world.enable(this);
 		this.scene.add.existing(this);
+
+		this.setVelocityX(this.direction * this.vx);
 	}
 
 	update(): void {
-		if (this.x > getGameWidth(this.scene) || this.x < 0 || 
-		this.y > getGameHeight(this.scene) || this.y < 0) {
-			this.destroy();
-			console.log('Projectile destroyed');
+		if (this.isOut()) {
+			this.destroy(true);
+			console.log("Projectile destroyed");
 		}
 	}
 }
