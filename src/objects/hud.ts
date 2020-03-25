325561441330ba5abb7a870e91200d6135c2a2ac
= <slotuser@gmail.com>
2020-03-25 16:15:48 +0100

    HUD: commented html struct about player's health.

--- src/objects/hud.ts
+++ src/objects/hud.ts
@@ -5,25 +5,25 @@
  */
 export class Hud {
 
-	private opacity: number;
-	private textSize: number;
-	private textColor: string;
 	private htmlStruct: string;
 
 	constructor(params) {
 
+		// I'm temporary including the health information here for now
+		// but ultimately a health bar will be implemented in Player class.
 		this.htmlStruct = `
 			<ul>
-				<li>${params.player.username}</li>
+				<li>${params.websiteUser.username}</li>
 				<li>
-					<img src="${params.player.avatar}">
+					<img src="${params.websiteUser.avatar}">
 				</li>
-				<li>${params.player.rank}</li>
+				<li>${params.websiteUser.rank}</li>
 			</ul>
 			<p>${params.player.health}</p>
 		`;
 
+		// 2nd argument is where phaser will put the html in, it's the container. 
+		// It's a div by default but I specify it anyway for clarity.
 		params.scene.add.dom().createFromHTML(this.htmlStruct, 'div');
-
 	}
 }
