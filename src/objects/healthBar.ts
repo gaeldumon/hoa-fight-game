33c96c9c74587709e1ced68b8c6387a6111befc0
= <slotuser@gmail.com>
2020-03-27 19:41:47 +0100

    Perfect tuning of the healthBar on the screen.

--- src/objects/healthBar.ts
+++ src/objects/healthBar.ts
@@ -2,28 +2,18 @@ export class HealthBar {
 
 	private bar: Phaser.GameObjects.Graphics;
 	private x: number;
-	private y: number;
+    private y: number;
+    private width: number;
+    private height: number;
 	private healthValue: number;
-	private percentage: number;
-
-    constructor(params: { scene: Phaser.Scene; x: number; y: number; }) {
-        this.bar = new Phaser.GameObjects.Graphics(params.scene);
-        this.x = params.x;
-        this.y = params.y;
-        this.healthValue = 100;
-        this.percentage = 76/100;
-
-        this.draw();
-
-        params.scene.add.existing(this.bar);
-    }
+    private percentage: number;
 
     public decrease(pAmount: number): boolean {
-		this.healthValue -= pAmount;
-		
-		if (this.healthValue < 0) {
-			this.healthValue = 0;
-		}
+        this.healthValue -= pAmount;
+
+        if (this.healthValue < 0) {
+            this.healthValue = 0;
+        }
 
         this.draw();
 
@@ -33,26 +23,35 @@ export class HealthBar {
     private draw(): void {
         this.bar.clear();
 
-        // Responsible for the black borders
-        this.bar.fillStyle(0x000000);
-        this.bar.fillRect(this.x, this.y, 80, 16);
-
-        // Responsible for the white background
+        // Background
         this.bar.fillStyle(0xffffff);
-        this.bar.fillRect(this.x + 2, this.y + 2, 76, 12);
+        this.bar.fillRect(this.x, this.y, this.width, this.height);
 
-		// Coloring the health jauge based on how much health you have left
-		// If your health is > 30 then the bar is green, if you have less it's red
-		if (this.healthValue > 30) {
-			this.bar.fillStyle(0x00ff00);
-		} else {
-			this.bar.fillStyle(0xff0000);
-		}
+        if (this.healthValue > 30) {
+            this.bar.fillStyle(0x00ff00);
+        } else {
+            this.bar.fillStyle(0xff0000);
+        }
 
         let healthWidth = Math.floor(this.percentage * this.healthValue);
-
 		// The actual health jauge you see decreasing
-        this.bar.fillRect(this.x + 2, this.y + 2, healthWidth, 12);
+        this.bar.fillRect(this.x, this.y, healthWidth, this.height);
     }
+
+    constructor(params) {
+        this.bar = new Phaser.GameObjects.Graphics(params.scene);
+        this.x = params.x;
+        this.y = params.y;
+        this.width = 90;
+        this.height = 12;
+        this.healthValue = 100;
+        this.percentage = 90/100;
+
+        this.draw();
+
+        params.scene.add.existing(this.bar);
+    }
+
+
 
 }
