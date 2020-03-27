f89781ed6e2466bbc10910cdfcfeb3c7b8370635
= <slotuser@gmail.com>
2020-03-27 19:42:11 +0100

    Not putting the healthbar inside the hud anymore.

--- src/objects/hud.ts
+++ src/objects/hud.ts
@@ -3,43 +3,47 @@
  * its avatar and rank (from website personal account).
  * Each HUD will be linked to its corresponding player (1 HUD instance per player).
  */
+
+import { WebsiteUser } from './websiteUser';
+
 export class Hud {
 
 	private htmlStruct: string;
 
-	constructor(params) {
+	constructor(params: { 
+		scene: Phaser.Scene;
+		x: number; 
+		y: number; 
+		color: string; 
+		websiteUser: WebsiteUser	
+	}) {
 
 		this.htmlStruct = `
 			<ul>
 				<li>${params.websiteUser.username}</li>
-				<li>
-					<img src="${params.websiteUser.avatar}">
-				</li>
+				<li><img src="${params.websiteUser.avatar}"></li>
 				<li>Rang: ${params.websiteUser.rank}</li>
 			</ul>
 			<style>
 				ul {
-					width: 90px;
-					padding: 10px;
-					border: 4px solid ${params.color};
+					width: 80px;
+					padding: 5px;
+					border: 3px solid ${params.color};
 					color: white;
 					font-weight: bold;
 					font-family: arial;
 					list-style-type: none;
 					text-align: center;
 				}
-
 				img {
 					opacity: .5;
+					padding: 5px 0px 5px 0px;
 				}
-			</style>
-		`;
+			</style>`;
 
 		// 2nd argument is where phaser will put the html in, it's the container. 
 		// It's a div by default but I specify it anyway for clarity.
-		params.scene.add.dom(params.x, params.y).createFromHTML(this.htmlStruct, 'div');
-		
-		// Purpose of add.existing for a dom element?
-		//params.scene.add.existing(this);
+		params.scene.add.dom(params.x, params.y)
+		.createFromHTML(this.htmlStruct, 'div');
 	}
 }
