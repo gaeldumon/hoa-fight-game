12bf9be2c63e5c7a3671bee0dbab8cae5bdf130b
= <slotuser@gmail.com>
2020-04-03 19:55:39 +0200

    Base skeleton of gameover scene.

---
+++ src/scenes/gameover-scene.ts
@@ -0,0 +1,27 @@
+
+
+const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
+	active: false,
+	visible: false,
+	key: 'Game'
+};
+
+
+export class GameoverScene extends Phaser.Scene {
+
+	constructor() {
+		super(sceneConfig);
+	}
+
+	preload() {
+
+	}
+
+	create() {
+
+	}
+
+	update() {
+
+	}
+}
