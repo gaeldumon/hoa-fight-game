ae2f74ab15be1b5d64c26411c726f9cdf3832306
= <slotuser@gmail.com>
2020-04-04 16:08:01 +0200

    Created boot scene with title and button linking to next scene.

--- src/scenes/boot-scene.ts
+++ src/scenes/boot-scene.ts
@@ -1,4 +1,6 @@
 import { getGameWidth, getGameHeight, COLORS } from '../helpers';
+import { WebsiteUser } from '../objects/websiteUser';
+
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 	active: false,
@@ -9,25 +11,67 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 
 export class BootScene extends Phaser.Scene {
 
+	private websiteUser1: WebsiteUser;
+	private websiteUser2: WebsiteUser;
+
+	private startBtn: Phaser.GameObjects.DOMElement;
+	private title: Phaser.GameObjects.DOMElement;
+
 	constructor() {
 		super(sceneConfig);
 	}
 
 	preload() {
+		/*this.load.pack(
+			"preload",
+			"assets/pack.json",
+			"preload"
+		);
 
+		for (let i = 1; i <= 6; i++) {
+			this.load.atlas(
+				`character${i}`,
+				`assets/images/characters/character${i}-spritesheet.png`,
+				`assets/images/characters/character${i}-atlas.json`
+			);
+		}*/
 	}
 
 	create() {
-		this.add.text(
-			getGameWidth(this)/2 - 155, 
-			getGameHeight(this)/2,
-			'Boot',
-			{
-				fontSize: '40px',
-				fontStyle: 'bold',
-				fill: COLORS.white.string
-			}
+
+		this.websiteUser1 = new WebsiteUser({
+			avatar: '',
+			username: 'Stan45',
+			rank: 1,
+			id: 15,
+			score: 20
+		});
+
+		this.websiteUser2 = new WebsiteUser({
+			avatar: '',
+			username: 'Beth347',
+			rank: 2,
+			id: 16,
+			score: 18
+		});
+
+		this.title = this.add.dom(
+			(getGameWidth(this)/2)-20,
+			50,
+			'h3',
+			'color: white; font-size: 45px; font-weight: bold; font-family: Grobold, Arial',
+			"HOA FIGHT"
 		);
+
+		this.startBtn = this.add.dom(
+			(getGameWidth(this)/2)-20,
+			(getGameHeight(this)/2),
+			'button',
+			'',
+			"Menu"
+		).addListener('click').on('click', () => {
+			this.scene.start('Menu');
+		});
 	}
 
 	update() {
