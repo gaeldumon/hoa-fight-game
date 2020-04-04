b6e9fd8a3089866afe10832668316e73728c3530
= <slotuser@gmail.com>
2020-04-04 16:09:48 +0200

    Added Boot, Menu and Gameover to scene index array.

--- src/scenes/index.ts
+++ src/scenes/index.ts
@@ -1,7 +1,11 @@
+import { BootScene } from './boot-scene';
+import { MenuScene } from './menu-scene';
 import { GameScene } from './game-scene';
 import { GameoverScene } from './gameover-scene';
 
 export default [
+	BootScene,
+	MenuScene,
 	GameScene,
 	GameoverScene
 ];
