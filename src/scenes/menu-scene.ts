db6bfba2ad3ab4da89e3da7d81c8f2eac7dacd8c
= <slotuser@gmail.com>
2020-04-04 16:10:11 +0200

    Menu with grobold title and start button to next scene (Game).

--- src/scenes/menu-scene.ts
+++ src/scenes/menu-scene.ts
@@ -1,4 +1,3 @@
-import { WebsiteUser } from '../objects/websiteUser';
 import { getGameWidth, getGameHeight, COLORS } from '../helpers';
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
@@ -10,6 +9,8 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 
 export class MenuScene extends Phaser.Scene {
 
+	private startBtn: Phaser.GameObjects.DOMElement;
+
 	constructor() {
 		super(sceneConfig);
 	}
@@ -19,16 +20,23 @@ export class MenuScene extends Phaser.Scene {
 	}
 
 	create() {
-		this.add.text(
-			getGameWidth(this)/2 - 100, 
-			getGameHeight(this)/2,
-			'Menu',
-			{
-				fontSize: '40px',
-				fontStyle: 'bold',
-				fill: COLORS.white.string
-			}
+		this.add.dom(
+			(getGameWidth(this)/2)-20,
+			50,
+			'h3',
+			'color: white; font-size: 45px; font-weight: bold; font-family: Grobold, Arial',
+			"Menu"
 		);
+
+		this.startBtn = this.add.dom(
+			(getGameWidth(this)/2)-20,
+			(getGameHeight(this)/2)-10,
+			'button',
+			'',
+			"Start Game"
+		).addListener('click').on('click', () => {
+			this.scene.start('Game');
+		});
 	}
 
 	update() {
