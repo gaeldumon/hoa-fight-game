e1e0612deb9f0f5ffd6699bd152e0ab7e5eeae62
= <slotuser@gmail.com>
2020-04-06 12:43:05 +0200

    Menu scene more modular part. 1, private methods encapsulation.

--- src/scenes/menu-scene.ts
+++ src/scenes/menu-scene.ts
@@ -1,5 +1,7 @@
-import { getGameWidth, getGameHeight, LEVEL_MIN, LEVEL_MAX, 
-CHARACTER_MIN, CHARACTER_MAX } from '../helpers';
+import { getGameWidth, getGameHeight, 
+	LEVEL_MIN, LEVEL_MAX, 
+	CHARACTER_MIN, CHARACTER_MAX } from '../helpers';
+
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 	active: false,
@@ -10,19 +12,52 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 
 export class MenuScene extends Phaser.Scene {
 
-	private startBtn: Phaser.GameObjects.DOMElement;
-	private background: Phaser.GameObjects.Image;
-	private title: Phaser.GameObjects.DOMElement;
-	private sectionTitle: Phaser.GameObjects.DOMElement;
 
-	private currentLevelImg: Phaser.GameObjects.Image;
-	private currentLevelId: number;
+	private background(): void {
+		this.add.image(
+			getGameWidth(this)/2, 
+			getGameHeight(this)/2,
+			'backgroundForGUIScenes'
+		);
+	}
 
-	private currentCharacterImg: Phaser.GameObjects.Image;
-	private currentCharacterId: number;
+	private logo(): void {
+		this.add.dom(
+			getGameWidth(this)/2,
+			30,
+			'h3',
+			'color:#fff; font-size: 55px; font-family: Grobold, Arial',
+			"HOA FIGHT"
+		);
+	}
 
-	private levelsBtn: Phaser.GameObjects.DOMElement;
-	private charactersBtn: Phaser.GameObjects.DOMElement;
+	private title(): void {
+		this.add.dom(
+			getGameWidth(this)/2,
+			120,
+			'h4',
+			'color:#fff; font-size: 30px; font-family: Grobold, Arial',
+			"Menu"
+		);
+	}
+
+	private startBtn(): void {
+		this.add.dom(
+			getGameWidth(this)/2,
+			getGameHeight(this)-50,
+			'button',
+			`width: 150px; 
+			height: 45px; 
+			font-family: Grobold,Arial; 
+			color: #000; 
+			font-size:25px; 
+			background-color: #d2d2d2; 
+			border:none`,
+			"Start"
+		).addListener('click').on('click', () => {
+			//this.scene.start('Game');
+		});
+	}
 
 	constructor() {		
 		super(sceneConfig);
@@ -44,79 +79,17 @@ export class MenuScene extends Phaser.Scene {
 		}
 	}
 
-	// Temporary code that will benefit a huge refactoring very soon
 	create() {
 
-		this.currentLevelId = LEVEL_MIN;
-		this.currentCharacterId = CHARACTER_MIN;
+		this.background();
+		this.logo();
+		this.title();
+		this.startBtn();
 
-		this.background = this.add.image(
-			getGameWidth(this)/2, 
-			getGameHeight(this)/2,
-			'backgroundForGUIScenes'
-		);
-
-		this.title = this.add.dom(
-			getGameWidth(this)/2,
-			30,
-			'h3',
-			'color:#fff; font-size: 55px; font-family: Grobold, Arial',
-			"HOA FIGHT"
-		);
-
-		this.sectionTitle = this.add.dom(
-			getGameWidth(this)/2,
-			120,
-			'h4',
-			'color:#fff; font-size: 30px; font-family: Grobold, Arial',
-			"Menu"
-		);
-
-		this.currentLevelImg = this.add.image(
-			250, 
-			350, 
-			`background${this.currentLevelId}Thumbnail`
-		);
-
-		this.levelsBtn = this.add.dom(250, 465, 'button', '', 'NEXT');
-		this.levelsBtn.addListener('click').on('click', () => {
-			if (this.currentLevelId < LEVEL_MAX) 
-				this.currentLevelId++;
-			else 
-				this.currentLevelId = LEVEL_MIN;
-			this.currentLevelImg.setTexture(`background${this.currentLevelId}Thumbnail`);
-		});
-
-		this.currentCharacterImg = this.add.image(
-			650,
-			350,
-			`character${this.currentCharacterId}Avatar`
-		);
-
-		this.charactersBtn = this.add.dom(650, 460, 'button', '', 'NEXT');
-		this.charactersBtn.addListener('click').on('click', () => {
-			if (this.currentCharacterId < CHARACTER_MAX) 
-				this.currentCharacterId++;
-			else 
-				this.currentCharacterId = CHARACTER_MIN;
-			this.currentCharacterImg.setTexture(`character${this.currentCharacterId}Avatar`);
-		});
-
-		this.startBtn = this.add.dom(
-			(getGameWidth(this)/2),
-			(getGameHeight(this)-50),
-			'button',
-			`width:150px; height:45px; font-family:Grobold,Arial; 
-			color:#000; font-size:25px; background-color:#d2d2d2; border:none`,
-			"Start"
-
-		).addListener('click').on('click', () => {
-
-			//this.scene.start('Game');
-
-		});
 	}
 
 	update() {
+
 	}
+
 }
