c88d8e4a811c74c8b3229c27d4f6608af2fea2e1
= <slotuser@gmail.com>
2020-04-07 15:02:13 +0200

    Menu: Big comments + instaured characterChoice and levelChoice.

--- src/scenes/menu-scene.ts
+++ src/scenes/menu-scene.ts
@@ -1,5 +1,8 @@
 import { getGameWidth, getGameHeight, GAMEDATA } from '../helpers';
+// To access the 2 public-static WebsiteUser instances of the Boot Scene.
 import { BootScene } from './boot-scene';
+// To declare expected "Choosing Buttons" argument type.
+import { WebsiteUser } from '../objects/websiteUser';
 
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
@@ -8,7 +11,12 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 	key: 'Menu'
 };
 
-
+/**
+ * This Scene is the Menu of the game, right before launching a match.
+ * It provides the "tuning" of the match: users can choose a map and each can
+ * choose a character. The Scene provides a Start Button with a callback action
+ * that starts the Game Scene...
+ */
 export class MenuScene extends Phaser.Scene {
 
 	private levelThumb: Phaser.GameObjects.Image;
@@ -16,7 +24,14 @@ export class MenuScene extends Phaser.Scene {
 	private characterThumbPlayer1: Phaser.GameObjects.Image;
 	private characterThumbPlayer2: Phaser.GameObjects.Image;
 	private charactersAvatarsTextures: Array<string>;
-
+	
+	// An id between 0 and GAMEDATA.LEVELS.NUMBER
+	private _levelChoice: number;
+	
+	public get levelChoice() : number {
+		return this._levelChoice;
+	}
+	
 	/**
 	 * Draws/Add the background (a Phaser Image)
 	 */
@@ -42,7 +57,7 @@ export class MenuScene extends Phaser.Scene {
 	}
 
 	/**
-	 * Add the scene title, for example: Menu (a Phaser DOM Element)
+	 * Add the h3 "scene title" (a Phaser DOM Element), for example here: "Menu"
 	 */
 	private title(): void {
 		this.add.dom(
@@ -54,6 +69,10 @@ export class MenuScene extends Phaser.Scene {
 		);
 	}
 
+	/**
+	 * Add a h4 "section title" (a Phaser DOM Element), for example "Maps" or
+	 * "Characters" : in short a section of the Menu GUI.
+	 */
 	private subTitle(x, y, text): void {
 		this.add.dom(
 			x,
@@ -64,7 +83,9 @@ export class MenuScene extends Phaser.Scene {
 		);
 	}
 
-	
+	/**
+	 * Add some "Hoa" styled text (i.e Grobold font, black, kinda big etc)
+	 */
 	private hoaText(x, y, text): void {
 		this.add.dom(
 			x,
@@ -76,7 +97,7 @@ export class MenuScene extends Phaser.Scene {
 	}
 
 	/**
-	 * Add the Start Button that starts the GameScene (and stops this one)
+	 * The Start Button that starts the GameScene (and stops this one)
 	 */
 	private startBtn(): void {
 		this.add.dom(
@@ -92,15 +113,44 @@ export class MenuScene extends Phaser.Scene {
 			border:none`,
 			"Start"
 		).addListener('click').on('click', () => {
+
 			//this.scene.start('Game');
+
 		});
 	}
 
-	private nextBtn(x, y, max, img: Phaser.GameObjects.Image, textures: Array<string>): void {
+	/**
+	 * Add a button in charge of SETTING A NEW TEXTURE of a Phaser image. 
+	 * Like a slideshow when you click the button "next" to see the next image.
+	 * On click it does 3 things: 
+	 * 1) Incrementing an index to pass through textures keys of an array one by
+	 * one until arrays's end is reached (then starting over).
+	 * 2) Each time we increment, a new texture is set on the image passed as 
+	 * argument based on the current texture key of the array.
+	 * 3) Appends a "character choice" in the form of the current texture key
+	 * to a websiteUser Instance (or field if declared public-static).
+	 * @param x: horizontal position of the button.
+	 * @param y: vertical position of the button.
+	 * @param img: displayed image, which texture's will change on button click.
+	 * @param textures: array composed of textures keys (see BootScene preload).
+	 * @param websiteUser: user that will get its characterChoice SET on click.
+	 */
+	private choosingBtn(
+		px: number, 
+		py: number, 
+		pImg: Phaser.GameObjects.Image, 
+		pTextures: Array<string>,
+		// If null that means this is the button for the level choosing.
+		// If not that means this is a button for a character choosing.
+		pWebsiteUser: WebsiteUser = null
+
+	): void {
+
 		let i = 0;
+
 		this.add.dom(
-			x,
-			y,
+			px,
+			py,
 			'button',
 			`width: 100px; 
 			height: 33px; 
@@ -111,8 +161,26 @@ export class MenuScene extends Phaser.Scene {
 			border:none`,
 			"Suivant"
 		).addListener('click').on('click', () => {
-			(i < max-1) ? i++ : i = 0;
-			img.setTexture(textures[i]);
+			// If we reach the end of the texture keys then going back to the
+			// first texture key, if not then going to the next texture key.
+			(i < pTextures.length - 1) ? i++ : i = 0;
+			// Setting the current texture key to the image, which in the end,
+			// acts as a container.
+			pImg.setTexture(pTextures[i]);
+
+			// Important piece of code. Appending a character choice to a user.
+			// If i = 0, then characterChoice is 0, then the user's player 
+			// instance will have the character0 (the red dude).
+			if (pWebsiteUser !== null) {
+				// An Id between 0 and GAMEDATA.CHARACTERS.NUMBER
+				pWebsiteUser.characterChoice = i;
+
+			} else {
+
+				this._levelChoice = i;
+
+			}
+
 		});
 	}
 
@@ -121,68 +189,60 @@ export class MenuScene extends Phaser.Scene {
 	}
 
 	preload() {
-		this.load.pack(
-			"preload",
-			"assets/pack.json",
-			"preload"
-		);
 
-		for (let i = 1; i <= 6; i++) {
-			this.load.atlas(
-				`character${i}`,
-				`assets/images/characters/character${i}-spritesheet.png`,
-				`assets/images/characters/character${i}-atlas.json`
-			);
-		}
 	}
 
 	create() {
 
+		// Putting all levels thumbnails TEXTURES KEYS inside an array, that
+		// we will pass through with the "Choosing Buttons" help and its "i" 
+		// index, on click. 
+		// All these textures keys come from the pack.json file.
 		this.levelsThumbsTextures = [];
-		for (let nLevel = 1; nLevel <= GAMEDATA.LEVELS.MAX; nLevel++) {
+		for (let nLevel = 0; nLevel < GAMEDATA.LEVELS.NUMBER; nLevel++) {
 			this.levelsThumbsTextures.push(`level${nLevel}Thumbnail`);
 		}
 
+		// Same for characters avatars textures keys.
 		this.charactersAvatarsTextures = [];
-		for (let nCharacter = 1; nCharacter <= GAMEDATA.CHARACTERS.MAX; nCharacter++) {
+		for (let nCharacter = 0; nCharacter < GAMEDATA.CHARACTERS.NUMBER; nCharacter++) {
 			this.charactersAvatarsTextures.push(`character${nCharacter}Avatar`);
 		}
 
 		this.background();
-		//this.logo();
 		this.title();
 		this.startBtn();
 		
+		// Terrain/map/level choosing section.
 		this.subTitle(240, 170, "Terrain");
 		this.levelThumb = this.add.image(240, 340, this.levelsThumbsTextures[0]);
-		this.nextBtn(
+		this.choosingBtn(
 			240, 
 			470, 
-			GAMEDATA.LEVELS.MAX,
 			this.levelThumb,
 			this.levelsThumbsTextures
 		);
-
+		
+		// Characters choosing section, divided in 2 sub-sections (1 per user).
 		this.subTitle(getGameWidth(this)-240, 170, "Personnages");
-
 		this.hoaText(700, 260, "Joueur 1");
 		this.characterThumbPlayer1 = this.add.image(700, 340, this.charactersAvatarsTextures[0]);
-		this.nextBtn(
+		this.choosingBtn(
 			700,
 			400,
-			GAMEDATA.CHARACTERS.MAX,
 			this.characterThumbPlayer1,
-			this.charactersAvatarsTextures
+			this.charactersAvatarsTextures,
+			BootScene.websiteUser1
 		);
 
 		this.hoaText(850, 260, "Joueur2");
 		this.characterThumbPlayer2 = this.add.image(850, 340, this.charactersAvatarsTextures[0]);
-		this.nextBtn(
+		this.choosingBtn(
 			850,
 			400,
-			GAMEDATA.CHARACTERS.MAX,
 			this.characterThumbPlayer2,
-			this.charactersAvatarsTextures
+			this.charactersAvatarsTextures,
+			BootScene.websiteUser2
 		);
 
 	}
