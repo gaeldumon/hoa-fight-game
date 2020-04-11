1d4c5c9135130ce06e146c4941bdde2291fe9a6b
= <slotuser@gmail.com>
2020-04-11 17:34:30 +0200

    Getter for score and avatar.

--- src/objects/websiteUser.ts
+++ src/objects/websiteUser.ts
@@ -1,8 +1,8 @@
  export class WebsiteUser {
 	 
 	private id: number;
-	private score: number;
-	private avatar: string;
+	private _score: number;
+	private _avatar: string;
 	private _username: string;
 	private _rank: number;
 	
@@ -13,12 +13,25 @@
 	public get rank(): number {
 		return this._rank;
 	}
+
+	public get avatar(): string {
+		return this._avatar;
+	}
+
+	public get score(): number {
+		return this._score;
+	}
+
+	public updateScore(): number {
+		this._score = this.score + 1;
+		return this._score;
+	}
 	
 	constructor(params: { avatar: string; username: string; rank: number; id: number; score: number; }) {
 		this.id = params.id;
 		this._username = params.username;
 		this._rank = params.rank;
-		this.score = params.score;
-		this.avatar = params.avatar;
+		this._score = params.score;
+		this._avatar = params.avatar;
 	}
  }
